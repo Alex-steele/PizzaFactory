c120edc590942dfc55b6e654fc3f069059a80ac6
Steele <69199527+Alex-steele@users.noreply.github.com>
2021-05-15 13:28:07 +0100

    Set up IOC container

--- PizzaFactory.ConsoleApp/PizzaFactoryServiceContainer.cs
+++ PizzaFactory.ConsoleApp/PizzaFactoryServiceContainer.cs
@@ -1,10 +1,35 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.DependencyInjection;
+using PizzaFactory.Core;
+using PizzaFactory.ConsoleApp.Runner;
+
 namespace PizzaFactory.ConsoleApp
 {
     public class PizzaFactoryServiceContainer
     {
+        private readonly IServiceProvider serviceProvider;
+
         public PizzaFactoryServiceContainer()
         {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var x = config["BaseCookingTime"];
+
+            var services = new ServiceCollection();
+
+            services.AddSingleton<IRandomPizzaGenerator>(_ => new RandomPizzaGenerator(int.Parse(config["BaseCookingTime"])));
+            services.AddSingleton<IPizzaFactoryRunner, PizzaFactoryRunner>();
+
+            serviceProvider = services.BuildServiceProvider();
+        }
+
+        public T GetService<T>()
+        {
+            return serviceProvider.GetService<T>();
         }
     }
 }
